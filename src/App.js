e1e7f1ecf792544fa739cde987abac07221a341f
Mohitnk <91474529+Mohitnk@users.noreply.github.com>
2022-05-04 12:58:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+import CountApp from "./CountApp/Count"
+function App() {
+  return (
+    <div>
+     <CountApp/>
+    </div>
+  );
+}
+
+export default App;
